a3fef52c5f12b6b67ceedf3696bdd069a5bcb45b
lbacaj <lbacaj@live.com>
2014-06-10 16:32:53 -0400

    first commit

---
+++ SingalHub/Hubs.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingalHub
+{
+    [HubName("tickerMini")]
+    public class TickerHub : Hub
+    {
+        private readonly StockTicker _stockTicker;
+
+        public TickerHub() : this(StockTicker.Instance) { }
+
+        public TickerHub(StockTicker stockTicker)
+        {
+            _stockTicker = stockTicker;
+        }
+
+        public IEnumerable<Stock> GetAllStocks()
+        {
+            return _stockTicker.GetAllStocks();
+        }
+
+        public string GetMarketState()
+        {
+            return _stockTicker.MarketState.ToString();
+        }
+
+        public void OpenMarket()
+        {
+            _stockTicker.OpenMarket();
+        }
+
+        public void CloseMarket()
+        {
+            _stockTicker.CloseMarket();
+        }
+
+        public void Reset()
+        {
+            _stockTicker.Reset();
+        }
+
+    }
+
+    [HubName("messageMini")]
+    public class MessageHub : Hub
+    {
+        public void SendMessage(string name, string message)
+        {
+            Clients.All.addMessage(name, message);
+        }
+    }
+}
---
+++ SingalHub/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(SingalHub.Startup))]
+
+namespace SingalHub
+{
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR();
+        }
+    }
+}
